21c9f625d1de840a8212bb586064a7a04fcca7be
duran <sakalli.duran@gmail.com>
2019-10-22 23:37:21 -0700

    Java Projects

--- JavaProjects/src/bankApplication/BankAccountApp.java
+++ JavaProjects/src/bankApplication/BankAccountApp.java
@@ -6,22 +6,7 @@ import java.util.List;
 public class BankAccountApp {
 	
 	public static void main(String[] args) {
-		
-		
-		
-		/*
-		
-		Checking checking1 = new Checking("Cro Dayi","3213321",2000);
-		
-		Saving saving1 = new Saving("Mehmet Emmi","42424243",2000);
-		checking1.compound();
-		checking1.showInfo();
-		
-		System.out.println("***************************************************************************");
-		saving1.compound();
-		saving1.showInfo();
-		
-		*/
+	
 		List<Account> accounts = new LinkedList<Account>();
 		String file = "C:\\Users\\Duran\\eclipse-workspace\\JavaProjects\\src\\bankApplication\\denem.csv";
 		List<String[]> newAccountHolders = utilities.CSV.read(file);
